f2b802a0b4c8963ac503b50b706994b1a8ce5866
gyerob <gyerob@gmail.com>
2021-11-16 23:00:38 +0100

    pulsoid integration done

--- js/pulsoid.js
+++ js/pulsoid.js
@@ -1,39 +1,36 @@
 var valasz;
-var oReq = new XMLHttpRequest();
+var req = new XMLHttpRequest();
 var hrm = document.getElementById("hrm");;
 var bpm;
+var timer;
 
-
-
-
-
-
-//oReq.addEventListener("load", reqListener);
-oReq.onreadystatechange = function () {
-	console.log("válasz");
-	//var valasz2 = JSON.parse(oReq.responseText);
-	//console.log(oReq.responseText);
+req.onreadystatechange = function () {
+	//console.log("válasz");
 	
-	if (oReq.readyState === XMLHttpRequest.DONE) {
-    console.log(typeof oReq.responseText);
-    console.log(oReq.responseText);
+	if (req.readyState === XMLHttpRequest.DONE) {
+    //console.log(typeof req.responseText);
+    //console.log(req.responseText);
 	
-	var valasz2 = JSON.parse(oReq.responseText);
-	for (const key in valasz2){
-		if(valasz2.hasOwnProperty(key)){
-			console.log(`${key} : ${valasz2[key]}`)
-			hrm.innerText = valasz2["bpm"];
+		valasz = JSON.parse(req.responseText);
+		for (const key in valasz){
+			if(valasz.hasOwnProperty(key)){
+				console.log(`${key} : ${valasz[key]}`)
+				hrm.innerText = valasz["bpm"];
+			}
 		}
+	}
 }
-  }
-	
-	
-	
+
+function startpulse(){
+	timer = setInterval(getpulsoid,500);
+	console.log("start timer");
 }
-oReq.open("GET", "https://pulsoid.net/v1/api/feed/5d6ebb36-5498-458a-b74f-ea73dc61a45a", true);
-oReq.setRequestHeader("Accept", "application/json");
-//oReq.responseType = 'json';
-oReq.send(null);
 
+function getpulsoid(){
+	req.open("GET", "https://pulsoid.net/v1/api/feed/5d6ebb36-5498-458a-b74f-ea73dc61a45a", true);
+	req.setRequestHeader("Accept", "application/json");
+	//req.responseType = 'json';
+	req.send(null);
+}				
 
-
+startpulse();
